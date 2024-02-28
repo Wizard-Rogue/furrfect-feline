524ef117b996e3eb5e7542b3d2745b41e1136817
Joseph Angelo Barrozo <jabarrozo@gmail.com>
2024-02-28 16:07:52 +0800

    added a very basic topbar
    Signed-off-by: Joseph Angelo Barrozo <jabarrozo@gmail.com>

---
+++ frontend/src/components/main/topbar/topbar.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Wrapper from './topbar.styles';
+
+function Topbar() {
+  return (
+    <Wrapper>
+      TOPBAR
+    </Wrapper>
+  )
+};
+
+export default Topbar;
