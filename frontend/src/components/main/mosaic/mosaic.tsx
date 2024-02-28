3651982f6874f8a7d95a80f66d2c73878545b222
Joseph Angelo Barrozo <jabarrozo@gmail.com>
2024-02-28 22:42:37 +0800

    updated mosaic component to dynamically load the pictures
    Signed-off-by: Joseph Angelo Barrozo <jabarrozo@gmail.com>

--- frontend/src/components/main/mosaic/mosaic.tsx
+++ frontend/src/components/main/mosaic/mosaic.tsx
@@ -1,13 +1,46 @@
-import React from 'react';
+import React, { useState, useEffect, useContext } from 'react';
+import SelectionContext, { SelectionContextType } from '../../../contexts/selectionContext';
 import MosaicList from './mosaic.styles';
 import Card from './card/card';
+import { CatsInfo } from '../../../types/shared';
 
 function Mosaic() {
+  const { selectedBreed } = useContext(SelectionContext) as SelectionContextType;
+  const [ catsInfo, setCatsInfo ] = useState<CatsInfo[]>([]);
+
+  async function loadCats () {
+    try {
+      const catsRequest = await fetch("/cats", {
+        method: "GET",
+      });
+  
+      if (!catsRequest.ok) {
+        console.error("Fetch failed!");
+        throw new Error("Apologies but we could not load new cats for you at this time! Miau!");
+      } else {
+        const catsData = await catsRequest.json() as CatsInfo[];
+        setCatsInfo([ ...catsInfo, ...catsData ]);
+      }
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  useEffect(() => {
+    loadCats();
+  }, [selectedBreed]);
+
   return (
     <MosaicList>
-      <Card id="1" image="https://cdn2.thecatapi.com/images/5rr.jpg" />
-      <Card id="2" image="https://cdn2.thecatapi.com/images/agm.jpg" />
-      <Card id="3" image="https://cdn2.thecatapi.com/images/b2r.png" />
+      {catsInfo.map((catInfo) => {
+        return (
+          <Card
+            key={catInfo.id}
+            id={catInfo.id}
+            image={catInfo.url}
+          />
+        )
+      })}
     </MosaicList>
   )
 };
