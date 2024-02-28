a1cec3415b82775da242ef011974d79c13bedf41
Joseph Angelo Barrozo <jabarrozo@gmail.com>
2024-02-28 22:41:59 +0800

    updated topbar with breed selection
    Signed-off-by: Joseph Angelo Barrozo <jabarrozo@gmail.com>

--- frontend/src/components/main/topbar/topbar.styles.ts
+++ frontend/src/components/main/topbar/topbar.styles.ts
@@ -1,7 +1,11 @@
 import styled from 'styled-components';
 
 const Wrapper = styled.div`
+  display: flex;
+  flex-flow: row wrap;
   text-align: center;
+  justify-content: space-around;
+  align-items: center;
 `;
 
 export default Wrapper;
--- frontend/src/components/main/topbar/topbar.tsx
+++ frontend/src/components/main/topbar/topbar.tsx
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { useLoaderData } from 'react-router-dom';
+import Form from 'react-bootstrap/Form';
+import SelectionContext, { SelectionContextType } from '../../../contexts/selectionContext';
 import Wrapper from './topbar.styles';
+import { BreedInfo } from '../../../types/shared';
 
 function Topbar() {
+  const { breedsData } = useLoaderData() as { breedsData: BreedInfo[] };
+  const { setSelectedBreed } = useContext(SelectionContext) as SelectionContextType;
+
+  function updateSelectedBreed (event: React.ChangeEvent<HTMLSelectElement>) {
+    const { target: { value } } = event;
+    setSelectedBreed(value)
+  }
+
   return (
     <Wrapper>
-      TOPBAR
+      <h3>Select a breed here:</h3>
+      <Form.Select onChange={(event) => updateSelectedBreed(event)}>
+        <option value=""></option>
+        {breedsData.map((breed) => {
+          return (
+            <option value={breed.id} key={breed.id}>{breed.name}</option>
+          );
+        })}
+      </Form.Select>
     </Wrapper>
   )
 };
