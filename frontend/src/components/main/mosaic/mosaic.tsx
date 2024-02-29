580b04d2587b2a19d60023334bbcb9a58ffa3819
Joseph Angelo Barrozo <jabarrozo@gmail.com>
2024-02-29 23:25:56 +0800

    implemented the load more button
    Signed-off-by: Joseph Angelo Barrozo <jabarrozo@gmail.com>

--- frontend/src/components/main/mosaic/mosaic.tsx
+++ frontend/src/components/main/mosaic/mosaic.tsx
@@ -1,11 +1,19 @@
 import React, { useContext } from 'react';
 import SelectionContext, { SelectionContextType } from '../../../contexts/selectionContext';
-import { MosaicList, Wrapper } from './mosaic.styles';
+import { MosaicList, Wrapper, StickyBottomWrapper } from './mosaic.styles';
 import Card from './card/card';
 import Topbar from '../topbar/topbar';
+import { Button } from 'react-bootstrap';
+import loadCats from '../catsLoader';
 
 function Mosaic() {
-  const { catsInfo } = useContext(SelectionContext) as SelectionContextType;
+  const { catsInfo, pageMetadata, setCatsInfo, setPageMetadata, selectedBreed } = useContext(SelectionContext) as SelectionContextType;
+
+  async function loadMore() {
+    const catsData = await loadCats(selectedBreed, pageMetadata.page + 1);
+    setCatsInfo([ ...catsInfo, ...catsData ]);
+    setPageMetadata({ page: pageMetadata.page + 1, disableLoadMore: !catsData || catsData.length !== 10 });
+  };
 
   return (
     <Wrapper>
@@ -21,6 +29,11 @@ function Mosaic() {
           )
         })}
       </MosaicList>
+      {!pageMetadata.disableLoadMore && (
+        <StickyBottomWrapper>
+          <Button variant="primary" disabled={pageMetadata.disableLoadMore} onClick={loadMore}>Load More</Button>
+        </StickyBottomWrapper>
+      )}
     </Wrapper>
   )
 };
