e16a2134685778c983151665d96906b3c29fc039
Joseph Angelo Barrozo <jabarrozo@gmail.com>
2024-03-01 16:52:26 +0800

    navigated error page back to the home page when dismissed
    Signed-off-by: Joseph Angelo Barrozo <jabarrozo@gmail.com>

--- frontend/src/components/error/error-page.tsx
+++ frontend/src/components/error/error-page.tsx
@@ -1,14 +1,15 @@
-import { useRouteError, isRouteErrorResponse } from "react-router-dom";
+import { useRouteError, isRouteErrorResponse, useNavigate } from "react-router-dom";
 import Alert from 'react-bootstrap/Alert';
 
 function ErrorPage() {
   const error = useRouteError();
+  const navigate = useNavigate();
   let message = isRouteErrorResponse(error) ? error.statusText : '';
   message = error instanceof Error ? error.message : message;
   console.error(error);
 
   return (
-    <Alert variant="danger" dismissible>
+    <Alert variant="danger" dismissible onClick={() => navigate('/')}>
       <Alert.Heading>Oops! An unexpected error has occurred.</Alert.Heading>
       <p>{message}</p>
     </Alert>
